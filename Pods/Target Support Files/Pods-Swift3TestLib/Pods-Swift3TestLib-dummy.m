870e24956a20c35441faa358abcf5bbd9eb1eee2
Louis de Decker <louis@supertoaster.be>
2016-11-03 21:48:33 +0100

    initial commit

---
+++ Pods/Target Support Files/Pods-Swift3TestLib/Pods-Swift3TestLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swift3TestLib : NSObject
+@end
+@implementation PodsDummy_Pods_Swift3TestLib
+@end
---
+++ Swift3TestLib/String+TestLib.swift
@@ -0,0 +1,18 @@
+//
+//  String+TestLib.swift
+//  Swift3TestLib
+//
+//  Created by Louis de Decker on 03/11/2016.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    
+    func double() -> String {
+        return "\(self)\(self)"
+    }
+    
+}
+
