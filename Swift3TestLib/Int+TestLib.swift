896e92ee4da04a335ff15b3176daa3137a7aa09b
Louis de Decker <louis@supertoaster.be>
2016-11-03 23:21:34 +0100

    adding Int

---
+++ Swift3TestLib/Int+TestLib.swift
@@ -0,0 +1,17 @@
+//
+//  Int+TestLib.swift
+//  Swift3TestLib
+//
+//  Created by Louis de Decker on 03/11/2016.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+import UIKit
+
+extension Int {
+    
+    func double() -> Int {
+        return self * 2
+    }
+}
+
